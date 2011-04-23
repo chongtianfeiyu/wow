456a4d443637f75173d78008020f97de45d9648d
zhang zhitong <zztztt109@gmail.com>
2011-04-23 14:11:05 +0800

    Enter

---
+++ wow_enter.php
@@ -0,0 +1,58 @@
+<html>
+    <head>
+        <title>mini wow</title>
+          <meta http-equiv="Content-Type" content="text/html; charset=utf8" />
+        <style type = "text/css">
+            body{
+                background-color:black;
+                text-align:center;
+            }
+            div.tf{
+                margin-left:30%;
+                margin-right:30%;
+                padding:5px ;
+                color:white;
+            }
+
+        </script>
+        </style>
+    </head>
+    <body>
+        <div>
+            <img src = "/wow/wowenter.png"/>
+        </div>
+        <div class = "tf">
+        <form action = "wow_enter.php" method = "post">
+            <a>帐号:</a>
+            <input type = "text" name = "id"/>
+            </br>
+            <a>密码:</a>
+            <input type = "password" name = "pw"/>
+            </br>
+            </br>
+            <input type = "button" value = "注册" onclick = "window.location = 'register.php'" />
+            <input type = "submit" value = "登陆"/>
+                <?php
+                session_start();
+                if(isset($_POST['id'])){
+                $link = mysql_connect('localhost','root','227227');
+                mysql_select_db("wow",$link);
+                mysql_query('SET NAMES UTF8');
+                $person_list = mysql_query('SELECT * FROM `person`',$link);
+                while($row_p = mysql_fetch_object($person_list)){
+                    
+                    if($row_p->ID == $_POST['id'] && $row_p->Password == $_POST['pw']){
+                        $hero = $row_p->Heroname;                       
+                        $_SESSION['person'] = $_POST['id'];
+                        $_SESSION['hero'] =  $hero;
+                        echo '<script type = "text/javascript"> document.location ="wow.php"</script>';                    
+                    }
+                }
+                echo '</br>帐号或密码错误';
+                }
+              ?>
+
+        </form>
+        </div>
+    </body>
+</html>
